c1d3a984ab0e40acc05da46c9e5882fa83285c7c
FFX11 <ffxfake@gmail.com>
2024-03-26 03:10:00 +0100

    update

--- app/embed/movie/[id]/page.tsx
+++ app/embed/movie/[id]/page.tsx
@@ -1,29 +1,29 @@
-'use client';
+// 'use client';
 
-import { useParams } from 'next/navigation';
-import axios from 'axios';
+// import { useParams } from 'next/navigation';
+// import axios from 'axios';
 
-export default function PostId(){
+// export default function PostId(){
 
-  const parms = useParams();
+//   const parms = useParams();
 
-  console.log(parms);
+//   console.log(parms);
 
-  //return <main>Post {parms?.id}</main>
+//   //return <main>Post {parms?.id}</main>
   
-}
+// }
 
-export const fetchData = async () => {
-  try {
+// export const fetchData = async () => {
+//   try {
     
-  const parms = useParams();
+//   const parms = useParams();
 
-    const response = await axios.get(`https://www.moviekex.online/movie/${parms?.id}`);
+//     const response = await axios.get(`https://www.moviekex.online/movie/${parms?.id}`);
 
-    console.log(response.status)
-    return response.data; // Return the fetched data
-  } catch (error) {
-    console.error('Error fetching movie data:', error);
-    throw new Error('Failed to fetch data');
-  }
-};
+//     console.log(response.status)
+//     return response.data; // Return the fetched data
+//   } catch (error) {
+//     console.error('Error fetching movie data:', error);
+//     throw new Error('Failed to fetch data');
+//   }
+// };
